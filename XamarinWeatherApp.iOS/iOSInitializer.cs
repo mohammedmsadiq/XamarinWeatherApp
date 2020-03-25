b42ee8f7c2ebe8af76eb8462f4d8ea19c052949a
Mohammed Sadiq <>
2020-03-25 21:37:01 +0000

    ios initializer added

---
+++ XamarinWeatherApp.iOS/iOSInitializer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Prism;
+using Prism.Ioc;
+
+namespace XamarinWeatherApp.iOS
+{
+    public class iOSInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // Register Any Platform Specific Implementations that you cannot 
+            // access from Shared Code
+        }
+    }
+}
