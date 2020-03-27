0bf47923dedac7dcdd474574841e6af86adcde2f
Mohammed Sadiq <>
2020-03-27 13:38:35 +0000

    no message

---
+++ XamarinWeatherApp.iOS/Renderers/ThemeRenderer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Intents;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XamarinWeatherApp.Styling;
+
+[assembly: ExportRenderer(typeof(ContentPage), typeof(XamarinWeatherApp.iOS.Renderers.ThemeRenderer))]
+namespace XamarinWeatherApp.iOS.Renderers
+{
+    public class ThemeRenderer : Xamarin.Forms.Platform.iOS.PageRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement != null || Element == null)
+            {
+                return;
+            }
+
+            try
+            {
+                SetTheme();
+            }
+            catch (Exception ex) { }
+        }
+
+        public override void TraitCollectionDidChange(UITraitCollection previousTraitCollection)
+        {
+            base.TraitCollectionDidChange(previousTraitCollection);
+
+            if (TraitCollection.UserInterfaceStyle != previousTraitCollection.UserInterfaceStyle)
+            {
+                SetTheme();
+            }
+        }
+
+        private void SetTheme()
+        {
+            if (TraitCollection.UserInterfaceStyle == UIUserInterfaceStyle.Dark)
+            {
+                App.Current.Resources = new DarkTheme();
+            }
+            else
+            {
+                App.Current.Resources = new LightTheme();
+            }
+        }
+    }
+}
+
---
+++ XamarinWeatherApp/Styling/FontFamily.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamarinWeatherApp.Styling
+{
+    public partial class FontFamily : ResourceDictionary
+    {
+        public FontFamily()
+        {
+            InitializeComponent();
+        }
+    }
+}
