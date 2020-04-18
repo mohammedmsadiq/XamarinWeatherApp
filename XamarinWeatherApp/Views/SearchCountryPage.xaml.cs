dc3a0d379a21161f33ea09b1a44b44e4445642c0
Mohammed Sadiq <>
2020-04-18 17:58:44 +0100

    no message

--- XamarinWeatherApp/Views/SearchCountryPage.xaml.cs
+++ XamarinWeatherApp/Views/SearchCountryPage.xaml.cs
@@ -16,7 +16,7 @@ namespace XamarinWeatherApp.Views
         {
             //Navi.FadeTo(0);
             pageHeight = height;
-            DetailSection.TranslationY = pageHeight;
+            //DetailSection.TranslationY = pageHeight;
             base.OnSizeAllocated(width, height);
         }
 
@@ -24,7 +24,7 @@ namespace XamarinWeatherApp.Views
         {
             //await Navi.FadeTo(0);
             await Task.Delay(Constants.Constants.AnimationDelay);
-            await DetailSection.TranslateTo(0, 0, 500, Easing.SinOut);
+            //await DetailSection.TranslateTo(0, 0, 500, Easing.SinOut);
             //await Navi.FadeTo(1, 500, Easing.SinIn);
             base.OnAppearing();
         }
